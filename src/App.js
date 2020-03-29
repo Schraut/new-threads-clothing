4a8156ca2437e39421e4fd8dcd4d22bbde785cf5
Dan Shrout <danshrout@yahoo.com>
2020-03-29 15:48:53 -0700

    integrate firebase into app, create google sign in and sign out button

--- src/App.js
+++ src/App.js
@@ -8,19 +8,43 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignOutPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
+import { auth } from './firebase/firebase.utils';
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignOutPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+      auth.onAuthStateChanged(user => {
+        this.setState({ currentUser: user});
+        
+        console.log(user);
+      })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignOutPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
